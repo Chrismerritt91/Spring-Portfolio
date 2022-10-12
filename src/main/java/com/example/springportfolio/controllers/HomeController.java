a12443aba0bac4fb41cb6bbabcaab0f571080908
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-10-12 15:35:25 -0500

    work on navigation and profile page

--- src/main/java/com/example/springportfolio/controllers/HomeController.java
+++ src/main/java/com/example/springportfolio/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.springportfolio.controllers;
 
+import com.example.springportfolio.model.Message;
 import com.example.springportfolio.model.User;
 import com.example.springportfolio.repositories.MessageRepository;
 import com.example.springportfolio.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,9 +27,17 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String Home() {
-//        model.addAttribute("user", new User());
+    public String Home(Model model) {
+        model.addAttribute("message", new Message());
         return "index";
+    }
+
+    @PostMapping("/messages/create")
+    public String insertMessage(@ModelAttribute Message message){
+        User user = userDao.findById(2L).get();
+        message.setUser(user);
+        messageDao.save(message);
+        return "redirect:/";
     }
 
 //    @PostMapping("/create-user")
--- src/main/java/com/example/springportfolio/controllers/ProfileController.java
+++ src/main/java/com/example/springportfolio/controllers/ProfileController.java
@@ -1,21 +1,42 @@
 package com.example.springportfolio.controllers;
 
+import com.example.springportfolio.model.Message;
 import com.example.springportfolio.model.User;
+import com.example.springportfolio.repositories.MessageRepository;
 import com.example.springportfolio.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class ProfileController {
 
+    private UserRepository usersDao;
+    private MessageRepository messagesDao;
+
+    public ProfileController(UserRepository usersDao, MessageRepository messagesDao) {
+        this.usersDao = usersDao;
+        this.messagesDao = messagesDao;
+    }
+
     @GetMapping("/profile")
-    public String Profile(){
+    public String Profile(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        long count = messagesDao.count();
+        List<Message> messages = messagesDao.findAll();
+        model.addAttribute("messages", messages);
+        model.addAttribute("count", count);
+        model.addAttribute("user", user);
         return "profile";
     }
+
 
 
 }
